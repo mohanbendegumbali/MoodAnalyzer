f600f0cbdc3e2315d1f089279ce5e693da74909f
mohanbendegumbali <mohan0311@hotmail.com>
2023-03-29 20:29:52 +0530

    Mood Analyser

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,25 @@
+public class MoodAnalyser {
+    private String message;
+
+    //Default Constructor
+    public MoodAnalyser(){
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    //Parameterized Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message.contains("Sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MoodAnalyserTest {
+
+    @Test
+    public void whenGivenMoodShouldReturnSAD(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals("SAD", mood);
+    }
+    @Test
+    public void whenGivenAnyMoodShouldReturnHAPPY(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("i am in Any Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals("HAPPY", mood);
+    }
+}
